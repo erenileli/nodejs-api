1cafd8df5df888152ef10ca5a569a757fbe5a1b6
Eren İleli <erenileli5561@gmail.com>
2021-11-21 22:07:08 +0300

    iki tarih arasına kadar ki routelar yazıldı

--- helper/db.js
+++ helper/db.js
@@ -10,5 +10,5 @@ module.exports = () => {
     });
 
     mongoose.Promise = global.Promise;
-}
+};
 
--- routes/movies.js
+++ routes/movies.js
@@ -23,20 +23,80 @@ router.post('/', (req, res, next) => {
   const movie = new Movie(
     req.body
   );
-  movie.save((err,data)=>{
-    if(err)
-      res.json(err);
-    else 
-      res.json(data)
-  });
-  // const promise = movie.save();
-  // promise.then((data)=>{
-  //   res.json({status : 1});
-  // })
-  // .cath((err)=>{
-  //   res.json(err);
+  // movie.save((err,data)=>{
+  //   if(err)
+  //     res.json(err);
+  //   else 
+  //     res.json(data)
   // });
 
+  // movie save
+   const promise = movie.save();
+  promise.then((data)=>{
+    res.json({status:1});
+  }).catch((err)=>{
+    res.json(err);
+  })
+
+});
+// top10 list
+router.get("/top10",(req,res,next)=>{
+  const promise = Movie.find({ }).limit(10).sort({imdb_score : -1});
+  promise.then((data)=>{
+    if(!data)
+      next({ message: 'The movie was not found.', code: 99 });
+    res.json(data);
+  }).catch((err)=>{
+    res.json(err);
+  })
+});
+// movie find 
+router.get("/:movie_id",(req,res,next)=>{
+  const promise = Movie.findById(req.params.movie_id);
+  promise.then((data)=>{
+    if(!data)
+      next({ message: 'The movie was not found.', code: 99 });
+    res.json(data);
+  }).catch((err)=>{
+    res.json(err);
+  })
+});
+// movie update movie
+router.put("/:movie_id",(req,res,next)=>{
+  const promise = Movie.findByIdAndUpdate(req.params.movie_id,req.body,{new:true});
+  promise.then((data)=>{
+    if(!data)
+      next({ message: 'The movie was not found.', code: 99 });
+    res.json(data);
+  }).catch((err)=>{
+    res.json(err);
+  })
+});
+// delete movie
+router.delete("/:movie_id",(req,res,next)=>{
+  const promise = Movie.findByIdAndDelete(req.params.movie_id,{new:true});
+  promise.then((data)=>{
+    if(!data)
+      next({ message: 'The movie is not found.', code: 99 });
+    res.json({status : "delete"});
+  }).catch((err)=>{
+    res.json(err);
+  })
+});
+
+// between 
+router.get("/between/:start_year/:end_year",(req,res,next)=>{
+  const {start_year,end_year} = req.params;
+  const promise = Movie.find({
+    year : { "$gte": parseInt(start_year), "$lte": parseInt(end_year)}
+   });
+  promise.then((data)=>{
+    if(!data)
+      next({message : 'The movie is not found'});
+    res.json(data);
+  }).catch((err)=>{
+      res.json(err);
+  });
 });
 
 module.exports = router;
